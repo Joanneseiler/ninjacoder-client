2b3f985ceb27458cdc6fc512f7be75958b4fe2fa
Anja Ramihone <AnjaRL@users.noreply.github.com>
2021-07-30 00:20:26 +0400

    searchbar ok

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 
 function SearchBar(props) {
   const { onSearch } = props;
 
   return (
-    <div style={{display: "flex", flexDirection: "row", justifyContent: "center", marginTop: 20}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 40,
+      }}
+    >
       <div>
-          <TextField style={{borderRadius: 20}}
-            onChange={onSearch}
-            id="standard-basic"
-            label="Search a course"
-            name="search"
-          />
+        <TextField
+          style={{ borderRadius: 20, width: "40ch" }}
+          onChange={onSearch}
+          id="standard-basic"
+          label="Search a course"
+          name="search"
+        />
       </div>
-      <div style={{paddingTop: 20}}>
-          <SearchIcon/>
+      <div style={{ paddingTop: 20 }}>
+        <SearchIcon />
       </div>
     </div>
   );
